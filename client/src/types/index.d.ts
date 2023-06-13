0e43a80e4dfd3e5a77567ec693ccf82c8019a0be
luiyezheng <yezhen.liu@uq.net.au>
2023-06-13 23:46:57 +0800

    declare yungou

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -12,6 +12,8 @@ export type PagingData<T> = {
 
 export type RequestPaging = { pageNum: number; pageSize: number; [key]: any };
 
+declare module 'yungouos-pay-node-sdk';
+
 declare global {
   var mongodb: Mongoose | string | null;
   var pgClient: Pool | null;
